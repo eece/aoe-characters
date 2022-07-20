39391b1506cba326bbf39062fbeb46047527028e
Emre Ece <eece90@gmail.com>
2022-07-21 00:54:33 +0300

    Add home image

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  homeImagePath: string = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.homeImagePath =  '../../assets/images/home.jpeg';
   }
 
 }
