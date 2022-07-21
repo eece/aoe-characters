82c10362a978bc34d6cdcb89501ea691b285eb20
Emre Ece <eece90@gmail.com>
2022-07-21 03:11:12 +0300

    Add update title on load unit-detail-page

--- src/app/pages/unit-detail-page/unit-detail-page.component.ts
+++ src/app/pages/unit-detail-page/unit-detail-page.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { changeTitle } from 'src/app/stateManagement/pageTitle.actions';
 
 @Component({
   selector: 'app-unit-detail-page',
   templateUrl: './unit-detail-page.component.html',
   styleUrls: ['./unit-detail-page.component.scss']
 })
-export class UnitDetailPageComponent implements OnInit {
+export class UnitDetailPageComponent implements OnInit{
 
-  constructor() { }
+  constructor(private store: Store<{ title: string }>) {
+    this.store.dispatch(changeTitle({value: 'Unit Details'}));
+   }
 
   ngOnInit(): void {
   }
